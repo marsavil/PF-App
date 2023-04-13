708d860e19a2d8118e9099ad039906a997bd8846
LucianoGalvarini <galvariniluciano@gmail.com>
2023-04-13 17:02:21 -0300

    update aside

--- Client/src/components/Order/Order.jsx
+++ Client/src/components/Order/Order.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "./order.scss";
 
 const Order = () => {
   return (
-    <div>
-      <h3>Ordenar</h3>
+    <div className="order">
+      <section className="orderSection">
+        <h4>Nombre:</h4>
+        <div>
+          <input type="radio" />
+          <label>Ascendente</label>
+        </div>
+        <div>
+          <input type="radio" />
+          <label>Descendente</label>
+        </div>
+      </section>
+      <section className="orderSection">
+        <h4>Precio:</h4>
+        <div>
+          <input type="radio" />
+          <label>Menor precio</label>
+        </div>
+        <div>
+          <input type="radio" />
+          <label>Mayor precio</label>
+        </div>
+      </section>
     </div>
   );
 };
