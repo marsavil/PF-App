602821ab667f75b3e70955d7f63904b3f9e76bba
LucianoGalvarini <galvariniluciano@gmail.com>
2023-04-18 17:25:43 -0300

    update register actions

--- Api/src/routes/UserRoutes.js
+++ Api/src/routes/UserRoutes.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const { registerUser, confirm, getUsers, logInUser, banUser, unBanUser, setAdminRightsToUser, removeAdminRightsToUser, createAdmin, updateUser, deleteUser } = require("../controlers/Users")
+const express = require("express");
+const {
+  registerUser,
+  confirm,
+  getUsers,
+  logInUser,
+  banUser,
+  unBanUser,
+  setAdminRightsToUser,
+  removeAdminRightsToUser,
+  createAdmin,
+  updateUser,
+  deleteUser,
+} = require("../controlers/Users");
 const router = express.Router();
 router.use(express.json());
 
 router.post("/", registerUser);
 router.get("/", getUsers);
 router.put("/setadmin", setAdminRightsToUser);
-router.put("/removeadmin", removeAdminRightsToUser)
+router.put("/removeadmin", removeAdminRightsToUser);
 router.post("/createadmin", createAdmin);
 router.post("/login/log", logInUser);
 router.put("/ban/:id", banUser);
 router.put("/unban/:id", unBanUser);
-router.get('/confirm/:token', confirm);
+router.get("/confirm/:token", confirm);
 router.put("/update", updateUser);
-router.get("/del", deleteUser)
-
-
+router.get("/del", deleteUser);
 
 module.exports = router;
-
-
-
