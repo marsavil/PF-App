ad857cd219477f392c6ef77c87ab3bf39305b3dd
LucianoGalvarini <galvariniluciano@gmail.com>
2023-04-19 11:09:50 -0300

    cart render and styles

--- Client/src/components/Cart/Cart.jsx
+++ Client/src/components/Cart/Cart.jsx
@@ -15,26 +15,6 @@ const Cart = () => {
     dispatch(getCart(id));
   }, []);
 
-  console.log(cartProducts);
-
-  //   {
-  //     "id": 2,
-  //     "name": "Apple Macbook Air (13 Pulgadas, 2020, Chip M1, 256 Gb De Ssd, 8 Gb De Ram) - Gris Espacial",
-  //     "brand": "Apple",
-  //     "price": 408000,
-  //     "image": "https://res.cloudinary.com/dlzp43wz9/image/upload/v1681426361/Products/02_yxyxpf.png",
-  //     "description": "La notebook más delgada y ligera de Apple viene con los superpoderes del chip M1. Termina todos tus proyectos mucho más rápido con el CPU de 8 núcleos y disfruta como nunca antes de apps y juegos con gráficos avanzados gracias al GPU de hasta 8 núcleos. Además, el Neural Engine de 16 núcleos se encarga de acelerar todos los procesos de aprendizaje automático. Todo en un diseño silencioso sin ventilador que te ofrece la mayor duración de batería en una MacBook Air: hasta 18 horas. (1) Portátil como siempre, más poderosa que nunca.\nAvisos Legales\nNo todos los recursos y configuraciones están disponibles en todos los países.\n(1) La duración de la batería varía según el uso y la configuración.\n(2) Comparado con la generación anterior.\n(3) El tamaño de la pantalla se mide en diagonal.",
-  //     "stock": 8,
-  //     "category": "Notebook",
-  //     "disable": false,
-  //     "ShoppingCart_Products": {
-  //         "id": 1,
-  //         "quantity": 16,
-  //         "ProductId": 2,
-  //         "ShoppingCartId": 1
-  //     }
-  // }
-
   return (
     <div className="cartProduct">
       <div className="cart-products">
