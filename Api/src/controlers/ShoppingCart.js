5af22ad705f2c0daf38dbdbdaea4bf34f0b0638b
Orianamartina <orianapettinelli04@gmail.com>
2023-04-18 21:22:23 -0300

    controlador shopping cart actualizado

--- Api/src/controlers/ShoppingCart.js
+++ Api/src/controlers/ShoppingCart.js
@@ -1,4 +1,4 @@
-const { ShoppingCart, Product, User } = require("../db");
+const {Product, User } = require("../db");
 
 module.exports = {
   addProductToShoppingCart: async function (productId, userId) {
@@ -10,23 +10,37 @@ module.exports = {
           id: userId,
         },
       });
-      user.getShoppingCart().then((cart) => {
+
+      const cart = await user.getShoppingCart()
+      const cartProducts = await cart.getProducts({ where: { id: productId } })
+      if (cartProducts.length) {
+        newQuantity = cartProducts[0].ShoppingCart_Products.quantity + 1;
+        await cart.addProduct(cartProducts[0], {
+          through: { quantity: newQuantity },});
+      }
+      else{
+        const productToAdd = await Product.findByPk(productId)
+        console.log(productToAdd)
+        await cart.addProduct(productToAdd, {
+          through: { quantity: newQuantity },});
+      }
+      
+      /*await user.getShoppingCart().then(async(cart) => {
         fetchedCart = cart;
-        cart
-          .getProducts({ where: { id: productId } })
-          .then((products) => {
+        await cart.getProducts({ where: { id: productId } })
+          .then((products) =>  {
             if (products.length) {
               newQuantity = products[0].ShoppingCart_Products.quantity + 1;
               return products[0];
             }
             return Product.findByPk(productId);
           })
-          .then((product) => {
-            return fetchedCart.addProduct(product, {
+          .then(async (product) => {
+            return  await fetchedCart.addProduct(product, {
               through: { quantity: newQuantity },
             });
           });
-      });
+      });*/
     } catch (error) {
       return error;
     }
