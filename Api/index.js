dc7350555dc07af07a280ce0a1e467987c4d5a08
Orianamartina <orianapettinelli04@gmail.com>
2023-04-21 15:05:57 -0300

    index

--- Api/index.js
+++ Api/index.js
@@ -8,11 +8,8 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 // Syncing all the models at once.
 
-<<<<<<< HEAD
-conn.sync({ force: false}).then(() => { 
-=======
+
 conn.sync({ force: false }).then(() => {
->>>>>>> 5b749103dfd87afd3542f9c073c3e7ed80479264
   server.listen(PORT, () => {
     serverAdmin();
     addProducts();
