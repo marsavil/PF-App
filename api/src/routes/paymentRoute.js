369ae5587b22514c87bf621349c07505861b9cd9
Francisco Achard <104811135+FranAchard@users.noreply.github.com>
2023-04-23 19:19:38 -0300

    Delete api directory

--- api/src/routes/paymentRoute.js
+++
@@ -1,7 +0,0 @@
-let express = require("express");
-let router = express.Router() ;
-const processPayment = require('../controlers/processPayment')
-
-router.post('/', processPayment)
-
-module.exports = router;
