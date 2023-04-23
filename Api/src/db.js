dee813022dd02a78bf5ba6061732b182177f8139
Francisco Achard <104811135+FranAchard@users.noreply.github.com>
2023-04-23 19:42:09 -0300

    Update db.js

--- Api/src/db.js
+++ Api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize, HasMany } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 //Conexion  a la Base de Datos
 
